acf637073762608acebe6cf5c1599eedc577767b
akova88 <phuoc.fiit@gmail.com>
2023-03-08 10:54:28 +0700

    use variabale

--- module_1/javascrip_base/main.js
+++ module_1/javascrip_base/main.js
@@ -1,1 +1,47 @@
-alert("Hi Javascrip basic!")
+// alert("Hi Javascrip basic!")
+// function showMessage () {
+//     alert('Xin chao!');
+// }
+
+// function showMessage() {
+//     document.getElementById('messegeDiv').innerHTML= 'Xin chào!';
+// }
+
+// function showMessage() {
+//     document.write('Xin chao');
+// }
+
+// function showMessage() {
+//     console.log('Xin chào');
+// }
+
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = 'Hà Nội';
+
+// document.write('i = ' + i);
+// document.write('</br>');
+// document.write('f = ' + f);
+// document.write('</br>');
+// document.write('b = ' + b);
+// document.write('</br>');
+// document.write('s = ' + s);
+
+// let width = 20;
+// let height = 10;
+// let area = width * height;
+
+// document.write('Diện tích Area=' + area);
+
+let a = prompt('Nhap so a:',);
+let b = prompt('Nhap so b',);
+ 
+
+ function divisor() {
+    let c = a % b;
+    if (c == 0) {
+    alert('a la boi so cua b');
+} else {alert('a ko phai la boi so cua b');}
+}
+divisor(a,b);
